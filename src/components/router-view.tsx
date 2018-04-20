e0d9dd499f8c98991de219c74a67ab0d7492960e
Naresh Bhatia <naresh.archfirst@gmail.com>
2018-04-20 18:48:53 -0400

    fix: Relaxed the type in ViewMap to allow any ReactNode not just React.Component

--- src/components/router-view.tsx
+++ src/components/router-view.tsx
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react';
 import { RouterStore } from '../router-store';
 
 export interface ViewMap {
-    [routeName: string]: React.Component;
+    [routeName: string]: React.ReactNode;
 }
 
 export interface RouterViewProps {
