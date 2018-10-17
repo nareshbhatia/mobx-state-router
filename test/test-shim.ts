b16494eb4deeec516eeed79d9ebe3a08ff74fec4
Naresh Bhatia <naresh.archfirst@gmail.com>
2018-10-17 19:46:38 -0400

    refactor: Updated dependencies

--- test/test-shim.ts
+++ test/test-shim.ts
@@ -1,9 +1,9 @@
 /**
- * Get rids of the missing requestAnimationFrame polyfill warning.
+ * Gets rid of the missing requestAnimationFrame polyfill warning.
  *
  * @link https://reactjs.org/docs/javascript-environment-requirements.html
  * @copyright 2004-present Facebook. All Rights Reserved.
  */
-global.requestAnimationFrame = function(callback) {
+(global as any).requestAnimationFrame = (callback: any) => {
     setTimeout(callback, 0);
 };
